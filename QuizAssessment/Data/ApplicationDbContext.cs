f4ce04c73b9eb324abf95a45c367981042010c94
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-04-16 16:06:04 +1000

    add ms sql server and create new table quizzes

---
+++ QuizAssessment/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizAssessment.Models;
+
+namespace QuizAssessment.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option): base(option)
+        {
+            
+        }
+        public DbSet<Quiz> Quizzes { get; set; }
+    }
+}
